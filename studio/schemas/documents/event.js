0eb59af5bb352c7410d819b39387f56d2925b5ab
Chris Coleman <freshyill@gmail.com>
2021-06-22 22:29:58 -0400

    sgb

--- studio/schemas/documents/event.js
+++ studio/schemas/documents/event.js
@@ -34,10 +34,56 @@ export default {
       "description": "Main image for this event. Appears on the event itself."
     },
     {
-      "name": null,
-      "type": null,
-      "title": null,
-      "description": null
-    }
-  ]
+      name: "body",
+      type: "bodyPortableText",
+      title: "Body",
+    },
+  ],
+  orderings: [
+    {
+      name: "publishingDateAsc",
+      title: "Publishing date new–>old",
+      by: [
+        {
+          field: "publishedAt",
+          direction: "asc",
+        },
+        {
+          field: "title",
+          direction: "asc",
+        },
+      ],
+    },
+    {
+      name: "publishingDateDesc",
+      title: "Publishing date old->new",
+      by: [
+        {
+          field: "publishedAt",
+          direction: "desc",
+        },
+        {
+          field: "title",
+          direction: "asc",
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: "title",
+      publishedAt: "publishedAt",
+      slug: "slug",
+      media: "mainImage",
+    },
+    prepare({ title = "No title", publishedAt, slug = {}, media }) {
+      const dateSegment = format(new Date(publishedAt), "yyyy/MM");
+      const path = `/${dateSegment}/${slug.current}/`;
+      return {
+        title,
+        media,
+        subtitle: publishedAt ? path : "Missing publishing date",
+      };
+    },
+  },
 }
